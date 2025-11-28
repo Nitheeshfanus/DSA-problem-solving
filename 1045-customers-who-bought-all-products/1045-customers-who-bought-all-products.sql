0bb07251009dd091b885034641835a5a397ddbe3
Nitheesh V <152017138+Nitheeshfanus@users.noreply.github.com>
2025-11-28 09:50:48 +0530

    Time: 548 ms (49.65%), Space: 0B (100%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with cte as(
+    select customer_id, count(distinct product_key) as cnt
+    from Customer
+    group by customer_id
+)
+select cte.customer_id as customer_id
+from cte 
+where cte.cnt= (select count(*) from Product);
