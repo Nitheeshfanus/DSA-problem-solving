be1ebe819938ba4077da375b773078504bd4e020
Nitheesh V <152017138+Nitheeshfanus@users.noreply.github.com>
2025-12-12 12:29:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.java
+++ Difficulty: Medium/Rod Cutting/rod-cutting.java
@@ -1,15 +1,20 @@
 class Solution {
     public int cutRod(int[] price) {
         // code here
-        int n=price.length;
-        int[] dp=new int[n+1];
-    
-        for(int i=0; i<price.length; i++){
-            for(int j=i+1; j<n+1; j++){
-                
-                dp[j]= Math.max(dp[j-(i+1)] + price[i], dp[j]);
+         int n = price.length;
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {          // i = piece length
+            for (int j = 1; j <= n; j++) {      // j = total rod length
+
+                dp[i][j] = dp[i - 1][j];        // don't use this piece
+
+                if (j >= i) {                   // use this piece
+                    dp[i][j] = Math.max(dp[i][j], price[i - 1] + dp[i][j - i]);
+                }
             }
         }
-        return dp[n];
+
+        return dp[n][n];
     }
 }
