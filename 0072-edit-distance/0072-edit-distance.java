64fe9a681b2031430ea0c7bc6c7bcfdae23919c5
Nitheesh V <152017138+Nitheeshfanus@users.noreply.github.com>
2025-12-15 19:42:24 +0530

    Time: 4 ms (73.27%), Space: 44.5 MB (97.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,24 +1,34 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n1= word1.length();
-        int n2=word2.length();
-        int[][] dp= new int[n1][n2];
-        //Arrays.fill(dp,-1);
-        return rec(n1-1,n2-1,word1,word2,dp);
-    }
-    public int rec(int i, int j, String word1, String word2, int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
+        int n = word1.length();
+        int m = word2.length();
 
-        if(dp[i][j]!=0) return dp[i][j];
+        int[] dp = new int[m + 1];
 
-        if(word1.charAt(i)==word2.charAt(j)){
-            return 0 + rec(i-1, j-1, word1, word2, dp);
+        // Base case: converting "" -> word2
+        for (int j = 0; j <= m; j++) {
+            dp[j] = j;
         }
-        
-        else{
-            return dp[i][j]=1+ Math.min(rec(i-1, j-1, word1, word2, dp),
-        Math.min(rec(i-1, j, word1, word2,dp),rec(i, j-1, word1, word2,dp)));
+
+        for (int i = 1; i <= n; i++) {
+            int prevDiag = dp[0]; // dp[i-1][j-1]
+            dp[0] = i;            // converting word1[0..i] -> ""
+
+            for (int j = 1; j <= m; j++) {
+                int temp = dp[j]; // save dp[i-1][j]
+
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[j] = prevDiag;
+                } else {
+                    dp[j] = 1 + Math.min(
+                        prevDiag,          // replace
+                        Math.min(dp[j],    // delete
+                                 dp[j - 1])// insert
+                    );
+                }
+                prevDiag = temp;
+            }
         }
+        return dp[m];
     }
 }
