83dafdd66903b7476184a0f9f8f869f530086d2b
Nitheesh V <152017138+Nitheeshfanus@users.noreply.github.com>
2026-02-09 18:21:27 +0530

    Time: 42 ms (37.35%), Space: 48 MB (33.96%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = p.length();
+        int n = s.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // empty pattern vs empty string
+        dp[0][0] = true;
+
+        // first column: pattern vs empty string
+        for (int i = 1; i <= m; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char pc = p.charAt(i - 1);
+                char sc = s.charAt(j - 1);
+
+                if (pc == sc || pc == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } 
+                else if (pc == '*') {
+                    // match empty OR consume one char
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } 
+                else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
