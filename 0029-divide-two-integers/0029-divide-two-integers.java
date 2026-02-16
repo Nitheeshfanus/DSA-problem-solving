16a55fa962aee52dce2ac490fe0f42ed29593947
Nitheesh V <152017138+Nitheeshfanus@users.noreply.github.com>
2026-02-16 20:38:07 +0530

    Time: 1 ms (90.29%), Space: 42.9 MB (28.93%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -3,7 +3,7 @@ class Solution {
         if (dividend == Integer.MIN_VALUE && divisor == -1)
             return Integer.MAX_VALUE;
 
-            boolean negative= (dividend < 0) ^ (divisor <0);
+            boolean negative= (dividend < 0) ^ (divisor < 0);
 
             long a=Math.abs((long)dividend);
             long b=Math.abs((long)divisor);
