ec8e88fe283e8c7903910a2729a48557dbe0a375
Nitheesh V <itshfanus@gmail.com>
2026-02-21 08:41:09 +0530

    Time: 22 ms (9.09%), Space: 41.9 MB (84.76%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,15 +1,33 @@
 class Solution {
+
+    int count = 1; // include 0
+
     public int countNumbersWithUniqueDigits(int n) {
-        if (n == 0)     return 1;
-        
-        int res = 10;
-        int uniqueDigits = 9;
-        int availableNumber = 9;
-        while (n-- > 1 && availableNumber > 0) {
-            uniqueDigits = uniqueDigits * availableNumber;
-            res += uniqueDigits;
-            availableNumber--;
+        if (n == 0) return 1;
+
+        boolean[] used = new boolean[10];
+
+        for (int i = 1; i <= 9; i++) {
+            used[i] = true;
+            backtrack(n, 1, used);
+            used[i] = false;
         }
-        return res;
+
+        return count;
+    }
+
+    private void backtrack(int n, int length, boolean[] used) {
+
+        count++;
+
+        if (length == n) return;
+
+        for (int i = 0; i <= 9; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                backtrack(n, length + 1, used);
+                used[i] = false;
+            }
+        }
     }
 }
