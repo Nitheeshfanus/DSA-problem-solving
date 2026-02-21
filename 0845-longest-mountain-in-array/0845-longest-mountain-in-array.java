dbcfdd83b02d7ff2530999e3b730d656803f46ee
Nitheesh V <itshfanus@gmail.com>
2026-02-21 12:33:18 +0530

    Time: 3 ms (79.26%), Space: 47.4 MB (73.87%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.java
@@ -1,12 +1,36 @@
 class Solution {
     public int longestMountain(int[] arr) {
-       int N = arr.length, res = 0;
-        int[] up = new int[N], down = new int[N];
-        for (int i = N - 2; i >= 0; --i) if (arr[i] > arr[i + 1]) down[i] = down[i + 1] + 1;
-        for (int i = 0; i < N; ++i) {
-            if (i > 0 && arr[i] > arr[i - 1]) up[i] = up[i - 1] + 1;
-            if (up[i] > 0 && down[i] > 0) res = Math.max(res, up[i] + down[i] + 1);
+        int n = arr.length;
+        int i = 1;
+        int maxLen = 0;
+
+        while (i < n - 1) {
+            
+            // Check if arr[i] is a peak
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                
+                int left = i - 1;
+                int right = i + 1;
+
+                // Expand left (climb)
+                while (left > 0 && arr[left] > arr[left - 1]) {
+                    left--;
+                }
+
+                // Expand right (descent)
+                while (right < n - 1 && arr[right] > arr[right + 1]) {
+                    right++;
+                }
+
+                maxLen = Math.max(maxLen, right - left + 1);
+                
+                // Move pointer to end of this mountain
+                i = right;
+            } else {
+                i++;
+            }
         }
-        return res; 
+
+        return maxLen;
     }
 }
