df8eea709180bba2b7ea862a03bc9a0143489597
Nitheesh V <itshfanus@gmail.com>
2026-03-30 19:58:07 +0530

    Time: 1 ms (88.18%), Space: 43.8 MB (95.93%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,23 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length;
-        List<List<Integer>> ans=new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
 
-        for(int mask=0; mask<(1<<n); mask++)
-        {
-            List<Integer> ss=new ArrayList<>();
-            for(int i=0; i<n; i++)
-            {
-                if((mask & (1<<i)) !=0){
-                    ss.add(nums[i]);
-                }
-            }
-            ans.add(ss);
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums, new ArrayList<>());
+        return result;
+    }
+
+    void dfs(int index, int[] nums, List<Integer> current) {
+        if (index == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
         }
-        return ans;
+
+        // include
+        current.add(nums[index]);
+        dfs(index + 1, nums, current);
+
+        // exclude
+        current.remove(current.size() - 1);
+        dfs(index + 1, nums, current);
     }
 }
